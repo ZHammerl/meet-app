e2d35077d72d6855432b0a48b6f98c09a1e8756b
Franziska Hammerl <fran.hammerl@gmail.com>
2022-09-30 10:35:12 +0200

    Updates

---
+++ precache-manifest.780f7dd3beda31766f271a76f2e952c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "846d60f9b306d005cfef",
+    "url": "/meet-app/static/css/main.b64cd189.chunk.css"
+  },
+  {
+    "revision": "846d60f9b306d005cfef",
+    "url": "/meet-app/static/js/main.846d60f9.chunk.js"
+  },
+  {
+    "revision": "4f473d823a9b02f8d234",
+    "url": "/meet-app/static/js/1.4f473d82.chunk.js"
+  },
+  {
+    "revision": "55cd605b8d650424cde5",
+    "url": "/meet-app/static/js/2.55cd605b.chunk.js"
+  },
+  {
+    "revision": "fa5ef916bcdfac0a82d2",
+    "url": "/meet-app/static/js/runtime~main.fa5ef916.js"
+  },
+  {
+    "revision": "800bdcf05565c433d2118e2e19fc5138",
+    "url": "/meet-app/index.html"
+  }
+];
