53bfa5173cec286f702b7ad7d6c0ebde7e9bde81
Franziska Hammerl <fran.hammerl@gmail.com>
2022-10-07 12:39:40 +0200

    Updates

--- precache-manifest.99a84b8a9ce74a497679c1b7f1094e4a.js
+++ precache-manifest.99a84b8a9ce74a497679c1b7f1094e4a.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "22d619e292e9f04e001d",
+    "revision": "28b7ef8c51c40f857306",
     "url": "/meet-app/static/css/main.49755f17.chunk.css"
   },
   {
-    "revision": "22d619e292e9f04e001d",
-    "url": "/meet-app/static/js/main.22d619e2.chunk.js"
+    "revision": "28b7ef8c51c40f857306",
+    "url": "/meet-app/static/js/main.28b7ef8c.chunk.js"
   },
   {
     "revision": "c639767f9224741d40f9",
     "url": "/meet-app/static/js/1.c639767f.chunk.js"
   },
   {
-    "revision": "274ea4391400848af8e9",
-    "url": "/meet-app/static/js/2.274ea439.chunk.js"
+    "revision": "d5fd7553e001c20ec74d",
+    "url": "/meet-app/static/js/2.d5fd7553.chunk.js"
   },
   {
     "revision": "b8627ab177700e3ed11d",
     "url": "/meet-app/static/js/runtime~main.b8627ab1.js"
   },
   {
-    "revision": "88d400fb59063902ca0319f532b13d0c",
+    "revision": "10a39270604680bb36c19ebcba28f68e",
     "url": "/meet-app/index.html"
   }
 ];
