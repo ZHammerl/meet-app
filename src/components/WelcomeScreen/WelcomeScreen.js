b0eecb5ae56048d6007bdfc13d9c3d6ffcd866ae
Franziska Hammerl <fran.hammerl@gmail.com>
2023-02-23 14:21:47 +0100

    add: message to welcome screen

--- src/components/WelcomeScreen/WelcomeScreen.js
+++ src/components/WelcomeScreen/WelcomeScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './WelcomeScreen.css';
+import React from "react";
+import "./WelcomeScreen.css";
 
 function WelcomeScreen({ getAccessToken }) {
   return (
@@ -7,8 +7,22 @@ function WelcomeScreen({ getAccessToken }) {
       <h1>Welcome to the Meet App</h1>
       <h4>
         Log in to see upcoming events around the world for
-        full-stack developers
+        full-stack developers.
       </h4>
+      <p>
+        There may be a warning from google that it is not
+        safe to continue. Don't let that scare you. This is
+        just because google classifies the app as a
+        "personal project" which will not be verified. I
+        promise it is absolutely safe and none of your data
+        will be retrieved.
+      </p>
+      <p>
+        No user information or personal calenders are
+        accessed, saved or used within the application. You
+        only need to log in because that whole OAuth login
+        is part of the study project.
+      </p>
       <div className="button_cont" align="center">
         <div className="google-btn">
           <div className="google-icon-wrapper">
@@ -29,7 +43,36 @@ o.svg"
           </button>
         </div>
       </div>
-      <a className='privacy-policy'
+      <p>
+        The Meet app is a pure study project. It only uses a
+        mock calender
+        (https://www.googleapis.com/auth/calendar.events.readonly).
+      </p>
+      <p>
+        The author is{" "}
+        <a
+          className="link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://Zhammerl.github.io/portfolio-website/index.html">
+          Franziska Hammerl
+        </a>
+        .
+      </p>
+      <p>
+        The source code of the app can be found in the{" "}
+        <a
+          className="link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/Zhammerl/meet-app">
+          Github repo
+        </a>
+        .
+      </p>
+
+      <a
+        className="privacy-policy"
         href="https://zhammerl.github.io/meet-app/privacy.html"
         rel="nofollow noopener">
         Privacy policy
